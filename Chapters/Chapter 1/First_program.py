923ade58fe00c8474e9bd822b1f998b7d33a6a6f
Unwana Friday Ezekiel <ezekiel.unwana@hotmail.com>
2024-09-13 17:19:47 +0200

    Challenge 1

---
+++ Chapters/Chapter 1/First_program.py
@@ -0,0 +1,15 @@
+# 1. Ask user for their first name
+first_name = input ('Enter your first name: ')
+
+# 2. Ask user for their last name
+last_name = input ('Enter your last name: ')
+
+# 3. Print out their full name
+print(f'Your full name is {first_name} {last_name}')
+
+# Or. 4 Print
+def Print(first_name, last_name):
+    pass
+
+
+Print(first_name, last_name)
