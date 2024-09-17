0fc8c5381e886e50c2524f215c15667ef1bc62ea
Unwana Friday Ezekiel <ezekiel.unwana@hotmail.com>
2024-09-17 19:51:58 +0200

    Challange_2

---
+++ Chapters/Chapter 1/challange_2.py
@@ -0,0 +1,10 @@
+#Create a Program that asks the user for a Season of the year
+season = input('Enter your favourite season')
+
+#Number of times something is done
+Number = input ('Enter the number of times you drink juice')
+
+#Adjective regarding the seasons
+adjective = input ('Enter the adjective')
+
+print(f'On a {adjective} {season} day, I drink at least {Number} cups of juice')
